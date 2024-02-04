fb61f5b7e86f624d4fc4704e548d60cc99688cbb
DVergaraM <daniel.vergaram@urosario.edu.co>
2024-02-03 22:52:19 -0500

    Delete main.cpp file and update .gitignore

---
+++ Llamadas/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int callType;
+    int duration;
+    int callValue = 0;
+    cout << "Choose an option: " << endl;
+    cout << "   1. National call" << endl;
+    cout << "   2. International call" << endl;
+    cout << "   3. Roaming call" << endl;
+    cin >> callType;
+    cout << "Enter the duration of the call (As an integer): ";
+    cin >> duration;
+
+    switch (callType)
+    {
+    case 1:
+        callValue = duration * 200;
+        break;
+    case 2:
+        callValue = duration * 500;
+        break;
+    case 3:
+        callValue = duration * 300;
+        break;
+    default:
+        cout << "Invalid option" << endl;
+        break;
+    }
+    cout << "The cost of your call is: " + to_string(callValue) << endl;
+    return 0;
+}
